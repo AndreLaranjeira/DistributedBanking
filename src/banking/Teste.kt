026e74ddb30c74a39939aaf0f6cb6c892a876ae6
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2021-04-24 17:33:43 -0300

    Modifications to test file * Added account passwords; * Removed warning from mutable variable usage;

--- src/banking/Teste.kt
+++ src/banking/Teste.kt
@@ -9,16 +9,16 @@ val listOfMyClassObject = object : TypeToken<ArrayList<Account>>() {}.type
 object Teste {
     data class state(
         var accounts: List<Account> = listOf(
-            Account("0", 1000.00),
-            Account("1", 2000.00),
-            Account("2", 3000.00),
-            Account("3", 4000.10),
+            Account("0", "senha", 1000.00),
+            Account("1", "senha", 2000.00),
+            Account("2", "senha", 3000.00),
+            Account("3", "senha", 4000.10),
         )
     )
 
     @JvmStatic
     fun main(args: Array<String>) {
-        var myState = state()
+        val myState = state()
 
         val serial = myState.toString()
         val json = Gson().toJson(myState)
